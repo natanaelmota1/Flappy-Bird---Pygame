f15b36f9608d43584cec1338a4a91f137876a248
ndmf.snf19@uea.edu.br <ndmf.snf19@uea.edu.br>
2019-11-05 21:25:28 -0400

    movimentação do pássaro pronta

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,5 +1,5 @@
 import pygame
-from random import randrange
+import random
 import os
 from datetime import datetime
 
@@ -10,22 +10,27 @@ except:
     print('O módulo pygame não foi inicializado com sucesso')
 
 # parâmetros da tela
-largura = 288
-altura = 512
+width = 288
+height = 512
 today = datetime.now()
 day = today.day
 month = today.month
 year = today.year
 
 # interface
-screen = pygame.display.set_mode((largura, altura))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('FLAPPY BIRD')
-relogio = pygame.time.Clock()
+FPS = pygame.time.Clock()
 background_day = pygame.image.load("sprites/background-day.png").convert()
 background_night = pygame.image.load("sprites/background-night.png").convert()
-sair = True
+exit = True
+count_updates = 0
 
-tupla_birds = (
+# parâmetros pássaro
+bird_x = 288/2
+bird_y = 512/2
+bird_sprite = 1
+tuple_birds = (
     # passarinho vermelho
     (
         'sprites/redbird-upflap.png',
@@ -45,17 +50,43 @@ tupla_birds = (
         'sprites/yellowbird-downflap.png',
     ),
 )
+# selecionando pássarinho aleatório
+rand_bird = random.randint(0,2)
+bird = (
+    pygame.image.load(tuple_birds[rand_bird][0]).convert_alpha(),
+    pygame.image.load(tuple_birds[rand_bird][1]).convert_alpha(),
+    pygame.image.load(tuple_birds[rand_bird][2]).convert_alpha(),
+)
 
-while(sair):
+while(exit):
     
     # escolha do background
     if (today.hour < 18):
         screen.blit(background_day, (0,0))
     else:
         screen.blit(background_night, (0,0))
-    pygame.display.update()
-    
-    # condição de fechamento do jogo
+
+    # animação passarinho
+    count_updates += 1
+    if (count_updates % 2 == 0):
+        bird_sprite = 1
+    elif (count_updates % 3 == 0):
+        bird_sprite = 2
+    else:
+        bird_sprite = 0
+    bird_y += 1.5
+    bird_player = screen.blit(bird[bird_sprite], (bird_x, bird_y))
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                bird_y -= 10
+
+    # condição de fechamento do jogo
         if (event.type == pygame.QUIT):
-            sair = False
+            exit = False
+        if (event.type == pygame.KEYDOWN):
+            if (event.key == pygame.K_ESCAPE):
+                exit = False
+    
+    pygame.display.update()
+    FPS.tick(20)
